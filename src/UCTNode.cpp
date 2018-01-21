94875ef53eb6c2c61473380fd87705dc718fcf0e
Eddh <remi.dufour@protonmail.com>
2018-01-21 20:06:00 +0100

    tune_fpu_m0.2_min0.01

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -295,7 +295,7 @@ float UCTNode::get_eval(int tomove) const {
         if (tomove == FastBoard::WHITE) {
             eval = 1.0f - eval;
         }
-        return eval;
+        return std::max(0.01f, eval-0.2f);
     }
 }
 
