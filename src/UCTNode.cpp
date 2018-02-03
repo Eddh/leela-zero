10da6922129d8277b095d1ca8d74ec33ccd895f9
Eddh <remi.dufour@protonmail.com>
2018-02-03 16:37:06 +0100

    ups

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -296,7 +296,7 @@ float UCTNode::get_eval(int tomove) const {
         if (tomove == FastBoard::WHITE) {
             eval = 1.0f - eval;
         }
-        return eval - cfg_fpu_reduction;
+        return eval;
     }
 }
 
